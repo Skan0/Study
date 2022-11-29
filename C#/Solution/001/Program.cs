bb13da07d60901e330d6ce0aa5124b9e266650bd
Skan0 <sytwj09110@naver.com>
2022-11-29 20:09:40 +0900

    첫날 1, 2, 3, 4

---
+++ C#/Solution/001/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//using MyConsole = System.Console;
+
+namespace _001_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            
+            if(args.Length > 0)
+                Console.WriteLine("arg: " + args[0]);
+        }
+    }
+}
---
+++ C#/Solution/002_Variable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수 선언의 기초 방법
+            {
+                int num = 10;
+                Console.WriteLine("num : " + num);
+            }
+
+            //변수 선언의 다양한 방법
+            {
+                int x;
+                int a, b, c;
+                int xx = 100;
+                int aa = 0, bb = 1, cc = 2;
+                
+            }
+        }
+    }
+}
