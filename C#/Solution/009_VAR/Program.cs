c8dfb1cd5f01dd38eed38d600bec216d418e168a
Skan0 <sytwj09110@naver.com>
2022-12-08 22:34:42 +0900

    8강

---
+++ C#/Solution/009_VAR/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _009_VAR
+{
+    internal class Program
+    {
+        //var umVar = 100;                   사용불가(멤버변수 사용불가)
+        //static var Func() { }              함수의 반환형으로 사용 불가
+        //static void Func(var a, var b)     함수의 파라미터로 사용불가
+        static void Main(string[] args)
+        {
+            var num = 0;
+            var numFVar = 0.0001f;
+
+            Console.WriteLine(num);
+            Console.WriteLine(numFVar);
+            
+            //var numNull = null;            null초기값은 사용불가
+
+
+        }
+    }
+}
---
+++ C#/Solution/010_Nullable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010_Nullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? num = 100;
+            double? dNum = 32123123.123123d;
+
+            Console.WriteLine($"num: {num}");
+            Console.WriteLine($"num: {num.Value}");
+            Console.WriteLine($"num: {num.HasValue}");
+            Console.WriteLine($"num: {num.GetType()}");
+            
+
+            Console.WriteLine($"dNum: {dNum}");
+            Console.WriteLine($"dNum: {dNum.Value}");
+            Console.WriteLine($"dNum: {dNum.HasValue}");
+            Console.WriteLine($"dNum: {dNum.GetType()}");
+
+
+            num = null;
+            num = num.GetValueOrDefault();
+            Console.WriteLine($"{num.HasValue}");
+            //if(num.HasValue)
+            //Console.WriteLine($"num: {num.GetType()}");
+
+            //변환
+            {// 명시적으로 형변환을 해줘야만 값이 들어가진다
+                int? nullNum = 100;
+                int numCast = (int)nullNum;
+            }
+        }
+    }
+}
